6662aecd6a1fc747dd25cfc2fdafa87ccd61c824
Thomas David <thomasdavid0@gmail.com>
2024-10-04 16:27:53 +0100

    fix routing

--- src/routes/FlightMenu.svelte
+++ src/routes/FlightMenu.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import NavMenu from "$lib/components/NavMenu.svelte";
+  import {base} from "$app/paths";
 </script>
 
 <NavMenu>
   <span slot="icon"><i class="bi bi-airplane"/></span>
-  <a class="dropdown-item" href='/analysis/upload/'>Load</a>
+  <a class="dropdown-item" href={base + '/analysis/upload/'}>Load</a>
   <div class="dropdown-item" >Import</div>
   <div class="dropdown-item" >Example</div>
 </NavMenu>
--- svelte.config.js
+++ svelte.config.js
@@ -14,8 +14,8 @@ const config = {
 			strict: true
 		}),
     paths: {
-      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-    },
+      base: process.env.NODE_ENV === 'production' ? '/PyFlightCoach/FlightCoachScore' : '',
+  },
     version: {name: process.env.npm_package_version}
 	}
 };
