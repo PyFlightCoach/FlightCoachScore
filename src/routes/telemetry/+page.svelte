8853aeeaca57e350bb51baeb4922aac5e7a65b39
Thomas David <thomasdavid0@gmail.com>
2025-02-04 12:10:51 +0000

    telemetry menu

--- src/routes/telemetry/+page.svelte
+++ src/routes/telemetry/+page.svelte
@@ -2,20 +2,39 @@
 	import Plot from 'svelte-plotly.js';
 	import { analysisServer } from '$lib/api/api';
 	import { user } from '$lib/stores/user';
+	import { saveAs } from 'file-saver';
 
 	let telemetry: Record<string, string[]> | undefined = $state();
 
-  if ($user?.is_superuser) {
-		analysisServer.get('telemetry').then((res) => {
-			telemetry = res.data;
-		});
-	}
+	const loadTelemetry = () => {
+		if ($user?.is_superuser) {
+			analysisServer.get('telemetry').then((res) => {
+				telemetry = res.data;
+			});
+		}
+	};
+  const saveTelemetry = () => {
+    if ($user?.is_superuser && telemetry) {
+      saveAs(new Blob([JSON.stringify(telemetry)], { type: 'application/json' }), 'telemetry.json');
+    }
+  };
 </script>
 
-<div class="col">
-	<Plot
-		data={[{ x: telemetry?.date?.map((d) => new Date(Date.parse(d))) || [], type: 'histogram' }]}
-		fillParent={true}
-    layout={{ title: 'Analysis Server Telemetry', xaxis: { title: 'Date' }, yaxis: { title: 'Analysed Manoeuvres' } }}
-	/>
+<div class="row">
+	<div class="col">
+		<Plot
+			data={[{ x: telemetry?.date?.map((d) => new Date(Date.parse(d))) || [], type: 'histogram' }]}
+			fillParent={true}
+			layout={{
+				yaxis: { title: 'Analysed Manoeuvres' }
+			}}
+		/>
+	</div>
 </div>
+
+<nav class="navbar navbar-expand-lg fixed-bottom">
+	<div class="container-fluid justify-content-center">
+		<button class="nav-item btn btn-outline-primary px-2" onclick={saveTelemetry} disabled={!telemetry}>Save</button>
+		<button class="nav-item btn btn-outline-primary px-2" onclick={loadTelemetry}>Refresh</button>
+	</div>
+</nav>
