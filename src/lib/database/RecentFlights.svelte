b23b413d8a61f70e419fe3a0d71ba7a246e1c689
Thomas David <thomasdavid0@gmail.com>
2025-10-22 11:21:41 +0100

    top flights, recent flights

--- src/lib/database/RecentFlights.svelte
+++ src/lib/database/RecentFlights.svelte
@@ -1,39 +1,47 @@
 <script lang="ts">
-	import type { DBFlightRanked } from "$lib/api/DBInterfaces/flight";
-  import {library} from "$lib/schedule/library";
+	import type { DBFlightRanked } from '$lib/api/DBInterfaces/flight';
+	import { library } from '$lib/schedule/library';
 
-  let {flightlist} : {flightlist: DBFlightRanked[]} = $props();
+	let { flightlist }: { flightlist: DBFlightRanked[] } = $props();
 
-  let scheduleSummary = $derived($library.summarize());
+	let scheduleSummary = $derived($library.summarize());
 
-  function getFlag(country: string) {
-    const data = country.split(" ");
-    return data[data.length-1];
-  }
+	function getFlag(country: string) {
+		const data = country.split(' ');
+		return data[data.length - 1];
+	}
 </script>
 
-
-	<div class="table-responsive rounded">
-		<table class="table table-striped text-center ">
-			<thead class="table-dark" style="z-index:-1">
-				<tr>
-						<th scope="col">Pilot</th>
-            <th scope="col">Schedule</th>
-            <th scope="col">Time</th>
-            <th scope="col">Comment</th>
-				</tr>
-			</thead>
-			<tbody>
-				{#each flightlist as row, i}
-					<tr
-						style="font-family: 'Twemoji Country Flags', sans-serif !important"
+<div class="table-responsive rounded">
+	<table class="table table-striped text-center">
+		<thead class="table-dark" style="z-index:-1">
+			<tr>
+				<th scope="col" class="text-start">Pilot</th>
+				<th scope="col">Schedule</th>
+				<th scope="col">Time</th>
+				<th scope="col" class="text-start">Comment</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each flightlist as row, i}
+				<tr style="font-family: 'Twemoji Country Flags', sans-serif !important">
+					<td class="text-start">{row.name} {getFlag(row.country)}</td>
+					<td>{scheduleSummary[row.schedule_id].repr.toUpperCase()}</td>
+					<td
+						>{new Date(row.datetime!).toLocaleString([], {
+							month: 'short',
+							day: 'numeric',
+							hour: '2-digit',
+							minute: '2-digit'
+						})}</td
 					>
-						<td>{row.name} {getFlag(row.country)}</td>
-            <td>{scheduleSummary[row.schedule_id].repr.toUpperCase()}</td>
-            <td>{new Date(row.datetime!).toLocaleString()}</td>
-            <td>{row.comment}</td>
-					</tr>
-				{/each}
-			</tbody>
-		</table>
-	</div>
+					<td class="px-0 overflow-hidden text-nowrap text-start" style="max-width: 150px">
+
+              {row.comment}
+            
+          </td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
--- src/lib/database/TopFlights.svelte
+++ src/lib/database/TopFlights.svelte
@@ -1,39 +1,37 @@
 <script lang="ts">
-  import type { DBFlightRanked } from '$lib/api/DBInterfaces/flight';
-  import {numberToPosition} from '$lib/utils/text';
-
+	import type { DBFlightRanked } from '$lib/api/DBInterfaces/flight';
+	import { numberToPosition } from '$lib/utils/text';
+  import TopFlightCell from '$lib/database/TopFlightCell.svelte';
 	let {
 		topflights
 	}: {
 		topflights: { schedule: string; count: number; flights: DBFlightRanked[] }[];
 	} = $props();
-
-
 </script>
 
-<div class="table-responsive rounded">
+<div class="table-responsive rounded text-center">
 	<table class="table table-striped">
 		<thead class="table-dark">
 			<tr>
-				<th>Schedule</th>
-        <th>Flight Count</th>
-        {#each topflights[0]?.flights as _, i}
-          <th>{numberToPosition(i + 1)}</th>
-        {/each}
+				<th class="text-start">Schedule</th>
+				{#each topflights[0]?.flights as _, i}
+					<th>{numberToPosition(i + 1)}</th>
+				{/each}
+				<th>Flight Count</th>
 				<!--<th colspan="2">Best Normalised</th>-->
 			</tr>
 		</thead>
 		<tbody>
 			{#each topflights as row, i}
-				
+				{#if row.count > 0}
 					<tr class="align-middle">
-						<td>{row.schedule}</td>
+						<th class="text-start">{row.schedule.toUpperCase()}</th>
+						<TopFlightCell flight={row.flights[0]} />
+						<TopFlightCell flight={row.flights[1]} />
+						<TopFlightCell flight={row.flights[2]} />
 						<td>{row.count}</td>
-						<td>{row.flights[0]}</td>
-						<td>{row.flights[1]}</td>
-						<td>{row.flights[2]}</td>
 					</tr>
-				
+				{/if}
 			{/each}
 		</tbody>
 	</table>
